5612b3330e1fb3efea99ed630e52042de99aed23
2102kh <144224944+2102kh@users.noreply.github.com>
2024-05-31 18:36:39 +0200

    added some styling

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <script>
 import EmployeeList from './components/EmployeeList.vue';
 export default {
-  name: 'App',
+ 
   components: {
     EmployeeList
   }
 }
 
-</script>
-
-  
-
-  
+</script>  
 <template>
   <div id="app">
+    <h1>Välkommen till Vendre!</h1>
+    <p>Vi är stolta över vårt team av begåvade och dedikerade medarbetrare som bidrar till vår framgång varje dag</p>
     <EmployeeList />
   </div>
 </template>
 
-
-<style>
-
+<style scoped>
+#app{
+padding:10% 5%;
+  width: 100vw;
+  height: 100vh;
+  /* background-color: #F7C0FE; */
+   background-color:rgb(238,235,252) ; 
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+h1{
+  color:rgb(83,51,237);
+}
 </style>
--- src/components/EmployeeList.vue
+++ src/components/EmployeeList.vue
@@ -18,14 +18,33 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <div v-for="employee in employees" :key="employee.id">
-      <p>{{ employee.first_name }} {{ employee.last_name }}</p>
+  <div class="employees-list">
+    <div v-for="employee in employees" :key="employee.id" class="employee-card">
       <img :src="employee.avatar"  />
+      <p>{{ employee.first_name }} {{ employee.last_name }}</p>
+      <a :href="`mailto:${employee.email}`">{{ employee.email }}</a>
     </div>
   </div>
 </template>
 
 <style scoped>
+.employees-list{
+  display: grid;
+  flex-wrap: wrap;
+  grid-template-columns: repeat(auto-fill, minmax(150px,1fr));
+  justify-items: center;
+  gap:30px;
+} 
+.employee-card{
+ padding-top: 20px;
+ a{
+  color:black;
+  font-size:700;
+ }
 
+
+}
+img{
+  border-radius: 50%;
+}
 </style>
